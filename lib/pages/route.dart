762a06a8634b73ac1cbf46b84446366794ff1cae
Patricia Cabiles <patricia.cabiles@atos.net>
2020-12-19 23:00:05 +0000

    add divider and change heading in route page

--- lib/pages/route.dart
+++ lib/pages/route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:keframa/shared/app_bar.dart';
+import 'package:keframa/shared/divider.dart';
 import 'package:keframa/shared/style.dart';
 
 class RoutePage extends StatefulWidget {
@@ -16,17 +17,23 @@ class _RoutePageState extends State<RoutePage> {
         appBar: AppBar(),
       ),
       body: Center(
-        child: Row(
+        child: Column(
           children: [
             Container(
-              padding: EdgeInsets.fromLTRB(120, 70, 300, 0),
+              padding: EdgeInsets.fromLTRB(120, 70, 120, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    'Route',
-                    style: headerStyle,
-                  )
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 120, 0),
+                        child: Text('The Route', style: headerStyle),
+                      ),
+                    ],
+                  ),
+                  CustomDivider(),
                 ],
               ),
             )
