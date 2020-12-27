909d2b79147adf79b387066202713505002cebc8
Patricia Cabiles <patricia.cabiles@atos.net>
2020-12-27 17:12:29 +0000

    extract homepage info

---
+++ lib/screens/home/assets/assets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:keframa/theme/style.dart';
+
+final mainIntro = Text(
+  "Today you've supported a child's education",
+  style: headerStyle,
+);
+
+final mainInfo = Text(
+  "A dedicated team of 56 cyclists are travelling 325 miles over 5 days in aid of the Keframa School Build. By supporting our cause, you have given a young adult in Northern Uganda a chance to thrive.",
+  style: textBodyStyle,
+);
